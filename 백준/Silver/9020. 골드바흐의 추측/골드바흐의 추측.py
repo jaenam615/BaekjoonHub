f51ebe1f5e5063b089df135e8df24c0bbd45fe8b
jaenam615 <155196757+jaenam615@users.noreply.github.com>
2024-01-13 00:32:00 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 4488 ms, Memory: 112188 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -0,0 +1,25 @@
+import sys
+N = int(input())
+a = [sys.stdin.readline() for j in range(N)]
+
+def isSosu(num):
+    result = False
+    count = 0
+    for i in range(1, int(num / 2)+1):
+        if num % i == 0:
+            count += 1
+    if count == 1:
+        return True
+    return result
+
+
+for i in a:
+    num = int(i)//2
+    A = num
+    B = num
+    while True:
+        if isSosu(A) & isSosu(B):
+            print(A,B)
+            break
+        A -= 1
+        B += 1
