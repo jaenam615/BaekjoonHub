e97ac7e4e2a1c643471812948c3a0842550b5c85
jaenam615 <155196757+jaenam615@users.noreply.github.com>
2024-01-12 20:58:55 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,15 @@
+T = int(input())
+x = 0
+y = 0
+z = 0
+
+while x < T:
+    R, S = input().split()
+    r = int(R)
+    while y < len(S):
+        result = ((S[y]) * r)
+        print(result,end="")
+        y += 1
+    print()
+    x += 1
+    y = 0
