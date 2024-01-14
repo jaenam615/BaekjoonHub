eede8516cdc259e774a3308f277723f6191a27d0
jaenam615 <155196757+jaenam615@users.noreply.github.com>
2024-01-14 16:16:11 +0900

    [Bronze II] Title: 수 정렬하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline())
+numbers = []
+i = 0
+
+
+while i < N:
+    numbers.append(int(sys.stdin.readline()))
+    i += 1
+
+numbers.sort()
+
+for number in numbers:
+    print(number)
