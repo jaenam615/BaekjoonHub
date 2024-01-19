dfce23ff2b9ec00dc00a715d458d93387255f307
jaenam615 <155196757+jaenam615@users.noreply.github.com>
2024-01-19 13:16:15 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 632 ms, Memory: 38304 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,29 +1,29 @@
 import sys
 
-sys.setrecursionlimit(10 ** 5)
-
 N, M = map(int, sys.stdin.readline().split())
 
 adj = [[0] * N for _ in range(N)]
 
 for i in range(M):
     a, b = map(int, sys.stdin.readline().split())
-    adj[a-1][b-1] = 1
-    adj[b-1][a-1] = 1
+    adj[a-1][b-1] = adj[b-1][a-1] = 1
 
-ans = 0
-chk = [False] * N
+ans = 0 
+chk = [False] * N 
 
 def dfs(now):
-    for nxt in range(N):
-        if adj[now][nxt] and not chk[nxt]:
-            chk[nxt] = True
-            dfs(nxt)
+    stk = []
+    stk.append(now)
+    while stk:
+        now = stk.pop()
+        for nxt in range(N):
+            if adj[now][nxt] and not chk[nxt]:
+                chk[nxt] = True
+                stk.append(nxt)
 
 for i in range(N):
     if not chk[i]:
-        ans += 1 
+        ans += 1
         chk[i] = True
-        dfs(i)
-
-print (ans)
+        dfs(i)    
+print(ans)
