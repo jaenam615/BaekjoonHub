5ddd80b789de2b925d86dc225333b1095a0e2a67
jaenam615 <155196757+jaenam615@users.noreply.github.com>
2024-01-22 15:02:46 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 264 ms, Memory: 49216 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,43 +1,42 @@
 import sys
-input = lambda: sys.stdin.readline().rstrip()
 
-V, E = map(int, input().split())
+V, E = map(int, sys.stdin.readline().split())
 
-# Kruskal Algorithm
-# https://techblog-history-younghunjo1.tistory.com/262
-edges = []
-for _ in range(E):
-    A, B, C = map(int, input().split())
-    edges.append((A, B, C))
-edges.sort(key=lambda x: x[2]) # C(Cost)가 적은 것부터 정렬
+edges = [0] * E
+
+for i in range(E):
+    edges[i] = tuple(map(int, sys.stdin.readline().split()))
+
+#가중치 오름차순으로 정렬
+edges.sort(key=lambda arr: arr[2])
 
 
-# Union-Find
+
 parent = [i for i in range(V+1)]
 
-def get_parent(x):
-    if parent[x] == x:
+def find_parent(arr, x):
+    if arr[x] == x:
         return x
-    parent[x] = get_parent(parent[x]) # get_parent 거슬러 올라가면서 parent[x] 값도 갱신
-    return parent[x]
+    arr[x] = find_parent(arr, arr[x])
+    return arr[x]
 
-def union_parent(a, b):
-    a = get_parent(a)
-    b = get_parent(b)
-
-    if a < b: # 작은 쪽이 부모가 된다. (한 집합 관계라서 부모가 따로 있는 건 아님)
-        parent[b] = a
+def union_parent(arr, a, b):
+    a = find_parent(arr, a) 
+    b = find_parent(arr, b) 
+    if a < b:
+        arr[b] = a
     else:
-        parent[a] = b        
+        arr[a] = b 
 
-def same_parent(a, b):
-    return get_parent(a) == get_parent(b)
+def same_parent(arr, a, b):
+    return find_parent(arr, a) == find_parent(arr, b)
 
+weight = 0
 
-answer = 0
 for a, b, cost in edges:
-    # cost가 작은 edge부터 하나씩 추가해가면서 같은 부모를 공유하지 않을 때(사이클 없을 때)만 확정
-    if not same_parent(a, b):
-        union_parent(a, b)
-        answer += cost
-print(answer)
+    # if parent[a] != parent[b]:
+    if not same_parent(parent, a,b):
+        union_parent(parent, a, b)
+        weight += cost
+
+print(weight)
