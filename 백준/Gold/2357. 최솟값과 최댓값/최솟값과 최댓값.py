e74f8ff75a6d4f802ede5b15fa203dff6756d729
jaenam615 <155196757+jaenam615@users.noreply.github.com>
2024-02-24 14:06:56 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 2428 ms, Memory: 51644 KB -BaekjoonHub

--- 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.py
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.py
@@ -1,53 +1,55 @@
-import math
 import sys
+import math
+sys.setrecursionlimit(10**8)
 
-sys.setrecursionlimit(10 ** 8)  # pypy 제출시 삭제!
-input = lambda: sys.stdin.readline().rstrip()
-# in_range = lambda y,x: 0<=y<n and 0<=x<m
+input = sys.stdin.readline
 
-
-def make_seg(idx, s, e):
-
-    if s == e:
-        seg[idx] = (arr[s], arr[s])  # min, max
-        return seg[idx]
-
-    mid = (s + e) // 2
-
-    l = make_seg(idx * 2, s, mid)
-    r = make_seg(idx * 2 + 1, mid + 1, e)
-
-    seg[idx] = (min(l[0], r[0]), max(l[1], r[1]))
-    return seg[idx]
+def make_tree(node, start, end):
+    if start == end:
+        tree[node] = (l[start],l[start])
+        return tree[node]
+    
+    mid = (start+end)//2
+    
+    left = make_tree((node*2),start,mid)
+    right = make_tree((node*2)+1,mid+1, end)
+    
+    tree[node] = (min(left[0], right[0]), max(left[1], right[1]))
+    return tree[node]
 
 
-def f(s, e, idx):
+def find_max(node, start, end):
+    if a > end or b < start:
+        return (1000000000,0)
 
-    # 탐색범위 s~e
-    if e < a or b < s:  # 범위 밖
-        return (1000000000, 0)
+    mid = (start+end)//2
 
-    mid = (s + e) // 2
-
-    if a <= s and e <= b:  # 탐색 범위가 작아서 다 리턴
-        return seg[idx]
-
+    if a <= start and end <= b:
+        return tree[node]
+    
     else:
-        l = f(s, mid, idx * 2)
-        r = f(mid + 1, e, idx * 2 + 1)
-        return (min(l[0], r[0]), max(l[1], r[1]))
+        left = find_max(node*2, start, mid)
+        right = find_max((node*2)+1, mid+1, end)
+        return (min(left[0], right[0]), max(left[1], right[1]))
 
 
-n, m = map(int, input().split())
-arr = [int(input()) for _ in range(n)]
 
-b = math.ceil(math.log2(n)) + 1
+
+
+N, M = map(int, input().split())
+l = []
+for _ in range(N):
+    l.append(int(input().rstrip()))
+    
+b = math.ceil(math.log2(N)) + 1
 node_n = 1 << b
-seg = [0 for _ in range(node_n)]
-make_seg(1, 0, len(arr) - 1)
 
-for _ in range(m):
-    a, b = map(int, input().split())
-    a, b = a - 1, b - 1  # idx
-    ans = f(0, len(arr) - 1, 1)
+tree = [0 for _ in range(node_n)]
+make_tree(1,0,N-1)
+
+for _ in range(M):
+    a, b = map(int, input().rstrip().split())
+    a = a-1
+    b = b-1
+    ans = find_max(1,0,N-1)
     print(ans[0], ans[1])
