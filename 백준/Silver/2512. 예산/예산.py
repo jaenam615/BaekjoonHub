85fbc74d53bc1a440ba68a124183c3088d43ffae
jaenam615 <jaenam2060@gmail.com>
2024-11-29 00:43:20 +0900

    [Silver II] Title: 예산, Time: 56 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -2,30 +2,38 @@
 # Online Python - IDE, Editor, Compiler, Interpreter
 
 
-def binary_search(arr, total):
+def binary_search(newArr, total):
     start = 0
-    end = max(arr)
+    end = max(newArr)
     target = 0
     
     while start <= end:
         mid = (start + end) // 2
+
         current = 0 
-        for i in range(len(arr)):
-            current += min(mid, arr[i])
-        
+        for i in range(len(newArr)):
+            current += min(mid, newArr[i])
         if current <= total:
             target = mid
+
             start = mid + 1 
         else:
             end = mid - 1
+            
     return target
 
 n = int(input())
 arr = list(map(int, input().split()))
 m = int(input())
 
+
 if sum(arr) <= m:
     print(max(arr))
 else:
-    print(binary_search(arr , m))
+    newArr = list(filter(lambda x: x > m//n, arr ))
+    if len(newArr) == 0:
+        print(max(arr))
+    else:
+        remainingPrice = m - (sum(arr) - sum(newArr))
+        print(binary_search(newArr, remainingPrice))
     
