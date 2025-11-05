325991e841c98abe3e4a332c899477f6fad703c4
jaenam615 <jaenam2060@gmail.com>
2025-11-06 05:04:53 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,34 +1,33 @@
 from collections import deque
 
+dy = (0, 1, 0, -1)
+dx = (1, 0, -1, 0)
+
 N, M = map(int, input().split())
 board = [input() for _ in range(N)]
 
-dx = (1, 0, -1, 0)
-dy = (0, 1, 0, -1)
-
-
 def is_valid_coord(y, x):
-    return 0 <= y < N and 0 <= x < M
-
+    return 0<=y<N and 0 <=x<M
 
 def bfs():
     chk = [[False] * M for _ in range(N)]
     chk[0][0] = True
     dq = deque()
-    dq.append((0, 0, 1))
+    dq.append((0,0,1))
     while dq:
-        y, x, d = dq.popleft()
-
-        if y == N - 1 and x == M - 1:
-            return d
-
-        nd = d + 1
+        y, x, count = dq.popleft()
+        
+        if y == N-1 and x == M-1:
+            return count
+        
         for k in range(4):
             ny = y + dy[k]
             nx = x + dx[k]
-            if is_valid_coord(ny, nx) and board[ny][nx] == "1" and not chk[ny][nx]:
+            if is_valid_coord(ny, nx) and board[ny][nx] == '1' and not chk[ny][nx]:
                 chk[ny][nx] = True
-                dq.append((ny, nx, nd))
-
+                dq.append((ny, nx, count + 1))
 
 print(bfs())
+
+
+
